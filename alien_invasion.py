22cee1a409c00fd085d571cc91740dcbef758332
Ademar De León Vázquez <ademardeleonvazquez@gmail.com>
2021-09-20 12:52:08 -0500

    The objects move faster when an entire fleet is shot down.

--- alien_invasion.py
+++ alien_invasion.py
@@ -62,21 +62,23 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
-        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
-        # Reset the game statistics.
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)        
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+            # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
 
-        # Get rid of any remaining aliens and bullets.
+            # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
             self.bullets.empty()
 
-        # Create a new fleet and center the ship
+            # Create a new fleet and center the ship
             self._create_fleet()
             self.ship.center_ship()
 
-        # Hide the cursor.
+            # Hide the cursor.
             pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
@@ -126,6 +128,7 @@ class AlienInvasion:
             # Destroy existing bullets.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """
